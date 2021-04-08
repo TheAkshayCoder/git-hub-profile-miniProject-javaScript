5b1000afd014a33556d26a5a7596f9044cc5a346
TheAkshayCoder <thakurakshay8375@gmail.com>
2021-04-08 17:04:13 +0530

    thired commit

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const reposDiv=document.getElementById("reposDiv")
 async function gitHubProfile(name){
     const response=await fetch(APIURL+name)
     const data = await response.json()
-    console.log(data)
     const newElm=document.createElement("div")
     newElm.classList.add("main")
     if (data.type!="User"){
@@ -14,14 +13,14 @@ async function gitHubProfile(name){
     }else{
         newElm.innerHTML=`
     <div class="image">
-        <a href="https://github.com/TheAkshayCoder" target="_blank"><img src="${data.avatar_url}" alt="image" srcset=""></a>
+        <a href="${data.html_url}" target="_blank"><img src="${data.avatar_url}" alt="image" srcset=""></a>
     </div>
     <div class="info">
                 <h1 class="login">${data.login}</h1>
                 <h3 class="created">${data.created_at}</h3>
                 <h3 class="followers">followers:${data.followers}</h3>
                 <h3 class="following">following:${data.following}</h3>
-                <h3 class="subscription">subscription: 0</h3>
+                <h3 class="subscription">subscription:</h3>
                 <h3 class="subscription">repos: ${data.public_repos}</h3>
                 <p class="bio">Bio:${data.bio}</p>
     </div>`
@@ -34,11 +33,13 @@ async function gitHubProfile(name){
 async function showRepo(name){
     const response=await fetch(APIURL+name+"/repos")
     const Data=await response.json()
-    // console.log(Data)
     const Elm=document.createElement("ul")
     Elm.classList.add("repos")
+    const heading=document.createElement("h1")
+        heading.innerText="Respositories"
+        reposDiv.appendChild(heading)
+    
     Data.forEach((data)=>{
-        console.log(data.name)
         const list=document.createElement("li")
         const anchor=document.createElement("a")
         list.appendChild(anchor)
